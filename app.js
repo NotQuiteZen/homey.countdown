cc4fd6d011d9c35ecb8d5ebe0d23b008fb39bc8d
Milan van As <milanvanas@gmail.com>
2018-12-04 20:54:17 +0000

    I think its alive

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {Timers} = require('./lib/timers');
+const {Timers, timerEvents} = require('./lib/timers');
 
 // Register actions
 const actionCountdownStop = require('./lib/actions/countdown_stop');
@@ -14,5 +14,16 @@ const conditionActive = require('./lib/conditions/countdown_active');
 const triggerCountdownStopped = require('./lib/triggers/countdown_stopped');
 const triggerSpecificCountdownStopped = require('./lib/triggers/specific_countdown_stopped');
 const triggerSpecificCountdownStarted = require('./lib/triggers/specific_countdown_started');
+
+// Bind triggers to timerEvents
+timerEvents.on('timer.started', (name) => {
+    let triggerTokens = {name: name};
+    triggerSpecificCountdownStarted.trigger(triggerTokens);
+});
+
+timerEvents.on('timer.finished', (name) => {
+    triggerCountdownStopped.trigger({name: name});
+    triggerSpecificCountdownStopped.trigger({}, name);
+});
 
 module.exports = Timers;
