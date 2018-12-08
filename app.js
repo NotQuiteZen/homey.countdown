64b83de89af20785632efaf746ede1e928f3d97f
Jeroen <jeroenbos22@gmail.com>
2018-12-08 08:34:37 +0100

    Revert "Update v1.1.1"

--- app.js
+++ app.js
@@ -1,29 +1,129 @@
 'use strict';
 
-const {Timers, timerEvents} = require('./lib/timers');
-
-// Register actions
-const actionCountdownStop = require('./lib/actions/countdown_stop');
-const actionCountdownStopAll = require('./lib/actions/countdown_stop_all');
-const actionCountdownUpdate = require('./lib/actions/countdown_update');
-
-// Register conditions
-const conditionActive = require('./lib/conditions/countdown_active');
+let timers = {};
+const Homey = require('homey');
 
 // Register triggers
-const triggerCountdownStopped = require('./lib/triggers/countdown_stopped');
-const triggerSpecificCountdownStopped = require('./lib/triggers/specific_countdown_stopped');
-const triggerSpecificCountdownStarted = require('./lib/triggers/specific_countdown_started');
+let specificCountdownStarted = new Homey.FlowCardTrigger('specific_countdown_started');
+specificCountdownStarted
+	.registerRunListener(( args, state ) => {
+		return Promise.resolve( (args.name && state.name) && args.name.toLowerCase() === state.name.toLowerCase() ); // If names match, this flow should run
+	})
+	.register()
 
-// Bind triggers to timerEvents
-timerEvents.on('timer.started', (name) => {
-    let triggerTokens = {name: name};
-    triggerSpecificCountdownStarted.trigger(triggerTokens);
-});
+let specificCountdownStopped = new Homey.FlowCardTrigger('specific_countdown_stopped');
+specificCountdownStopped
+	.registerRunListener(( args, state ) => {
+			return Promise.resolve( (args.name && state.name) && args.name.toLowerCase() === state.name.toLowerCase() ); // If names match, this flow should run	
+	})
+	.register()
+	
 
-timerEvents.on('timer.finished', (name) => {
-    triggerCountdownStopped.trigger({name: name});
-    triggerSpecificCountdownStopped.trigger({}, name);
-});
 
-module.exports = Timers;
+// Register conditions
+let countdownActive = new Homey.FlowCardCondition('countdown_active');
+countdownActive
+	.register()
+	.registerRunListener(( args, state ) => {
+		if (/^[a-zA-Z0-9 ]+$/.test(args.name)) {
+			return Promise.resolve( (args.name.toLowerCase() in timers) );	
+		}
+		return Promise.reject( new Error('Invalid name, only use a-z and 0-9') );
+	})
+	
+	
+	
+// Register actions
+let countdownUpdate = new Homey.FlowCardAction('countdown_update');
+countdownUpdate
+	.register()
+	.registerRunListener(( args ) => {
+		if (/^[a-zA-Z0-9 ]+$/.test(args.name)) {
+			if (args.seconds > 0) {
+				console.log('Started timer '+args.name);
+				timers[args.name.toLowerCase()] = args.seconds;
+				
+				specificCountdownStarted.trigger({}, {'name': args.name});
+				
+				let countdownStarted= new Homey.FlowCardTrigger('countdown_started');			
+				let tokens = {
+					'name': args.name
+				}
+				countdownStarted
+					.register()
+					.trigger(tokens)
+				
+				return Promise.resolve( true );
+			}
+			return Promise.reject( new Error('Seconds must be greater then 0') );	
+		}
+		return Promise.reject( new Error('Invalid name, only use a-z and 0-9') );
+	});
+	
+let countdownStop = new Homey.FlowCardAction('countdown_stop');
+countdownStop
+	.register()
+	.registerRunListener(( args ) => {
+		if (/^[a-zA-Z0-9 ]+$/.test(args.name)) {
+			if (args.name.toLowerCase() in timers) {
+				timers[args.name.toLowerCase()] = 0;
+			}
+			return Promise.resolve( true );
+		} 
+		return Promise.reject( new Error('Invalid name, only use a-z and 0-9') );
+	});
+
+let countdownStopAll = new Homey.FlowCardAction('countdown_stop_all');
+countdownStopAll
+	.register()
+	.registerRunListener(() => {
+		console.log('Stopping all timers');
+		Object.keys(timers).forEach(function(key) {
+			timers[key] = 0;
+		});
+		
+		return Promise.resolve( true );
+	});
+	
+let countdownStopped= new Homey.FlowCardTrigger('countdown_stopped');			
+countdownStopped
+	.register()
+	
+
+class CountdownTimers extends Homey.App {
+	
+	onInit() {
+		this.log('Countdown timers is running...');
+		
+		this.updateTimers();
+	}
+	
+	updateTimers() {
+		if (Object.keys(timers).length > 0) {
+			console.log(timers);	
+		}
+		
+		Object.keys(timers).forEach(function(key) {
+			var timeLeft = timers[key];
+			
+			if (timeLeft > 0) {
+				timers[key] = timeLeft - 1;
+			} else {
+				delete timers[key];
+				console.log('Timer '+key+' has stopped');
+				
+				specificCountdownStopped.trigger({}, {'name': key}); // Trigger specific_countdown_stopped
+				
+				let tokens = {
+					'name': key
+				}
+				countdownStopped
+					.trigger(tokens) // Trigger countdown_stopped
+			}
+		  });
+		
+		setTimeout(() => this.updateTimers(), 1000);
+	}
+}
+
+module.exports = CountdownTimers;
