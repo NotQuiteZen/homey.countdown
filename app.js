5c2bd50e48fa158d1cd33fdee37037a1b2a54b50
Milan van As <milanvanas@gmail.com>
2018-12-18 21:18:37 +0000

    Added autocomplete, currently copied cards so we can slowly phase out the non-autocomplete versions

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ const {Timers} = require('./lib/timers');
 
 // Register actions
 const actionCountdownStop = require('./lib/actions/countdown_stop');
+const actionCountdownStopAutocomplete = require('./lib/actions/countdown_stop_autocomplete');
 const actionCountdownStopAll = require('./lib/actions/countdown_stop_all');
 const actionCountdownUpdate = require('./lib/actions/countdown_update');
+const actionCountdownUpdateAutocomplete = require('./lib/actions/countdown_update_autocomplete');
 
 // Register conditions
 const conditionActive = require('./lib/conditions/countdown_active');
+const conditionActiveAutocomplete = require('./lib/conditions/countdown_active_autocomplete');
 
 // Register triggers
 const triggerCountdownStarted = require('./lib/triggers/countdown_started');
 const triggerCountdownStopped = require('./lib/triggers/countdown_stopped');
 const triggerSpecificCountdownStopped = require('./lib/triggers/specific_countdown_stopped');
+const triggerSpecificCountdownStoppedAutocomplete = require('./lib/triggers/specific_countdown_stopped_autocomplete');
 const triggerSpecificCountdownStarted = require('./lib/triggers/specific_countdown_started');
+const triggerSpecificCountdownStartedAutcomplete = require('./lib/triggers/specific_countdown_started_autocomplete');
+
+
+// Give Timers all the cards, so we can do all kinds of stuff
+Timers.setCards([
+    actionCountdownStop,
+    actionCountdownStopAll,
+    actionCountdownUpdate,
+    conditionActive,
+    triggerCountdownStarted,
+    triggerCountdownStopped,
+    triggerSpecificCountdownStopped,
+    triggerSpecificCountdownStarted,
+    actionCountdownUpdateAutocomplete,
+    actionCountdownStopAutocomplete,
+    triggerSpecificCountdownStoppedAutocomplete,
+    triggerSpecificCountdownStartedAutcomplete,
+]);
 
 module.exports = Timers;
